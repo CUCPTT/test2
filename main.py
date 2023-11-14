4a117536497adba69eae9e33ed2990a8d52ab2ef
layla <3362899143@qq.com>
2023-11-14 16:09:52 +0800

    加入输入为空的错误检查

--- main.py
+++ main.py
@@ -3,19 +3,12 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from PIL import Image
 import re
-# import graphviz
+import graphviz
 from matplotlib import pyplot as plt
 from infix2postfix import infix_to_postfix
 from postfix2nfa import str_to_nfa,generate_nfa
 from NFA_to_DFA import nfa_to_dfa
-from DFA_minimize import operation
-
-# infix_expression = '(a*|b*|c*)*'
-# postfix_expression = infix_to_postfix(infix_expression)
-# print(postfix_expression)
-# nfa_instance = str_to_nfa(postfix_expression)
-# nodes,edges = generate_nfa(nfa_instance)
-
+from DFAMinimize import operation
  
 def create_graph(nodes, edges):
     # 创建有向图并设置布局方向为水平
@@ -71,6 +64,11 @@ def Min_DFA(input_text):
 def submit_button_clicked():
     regex = regex_entry.get()
     option = option_combobox.get()
+
+    # 检查正则表达式是否为空
+    if not regex:
+        messagebox.showerror('Empty regular expression', 'Please enter a regular expression.')
+        return
 
     # 检查正则表达式是否有效
     try:
